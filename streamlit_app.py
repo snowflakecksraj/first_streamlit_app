644daedeece8a4e7982ccb5b41d0ba4b2f409444
Selvaraj Pandian <119549934+snowflakecksraj@users.noreply.github.com>
2022-12-05 13:57:05 +0530

    Update streamlit_app.py
    FRUIT_LOAD_LIST

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import pandas
 import requests
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
@@ -38,4 +35,20 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from FRUIT_LOAD_LIST")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains:")
+steamlit.text(my_data_row)
+
+
+
+
+
    
